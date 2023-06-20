12700599771f305a58daeb6b4ae6733a07682cfc
nithin <to.nithinv@gmail.com>
2023-06-20 15:22:21 +0530

    web dev

---
+++ web/resources/store.py
@@ -0,0 +1,8 @@
+import uuid
+from flask import request 
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from db import stores,items
+
+
+
---
+++ web/test.py
@@ -0,0 +1,14 @@
+# A = {
+#     "id1": [],
+#     "id2": {"name": "pencil", "price": 10},
+#     "id3": {"name": "barbie", "price": 350},
+# }
+
+# item = A["id1"]
+# # item|= {"name": "book", "price":5}
+
+# print(A)
+
+import uuid
+
+print(uuid.uuid4().hex)
